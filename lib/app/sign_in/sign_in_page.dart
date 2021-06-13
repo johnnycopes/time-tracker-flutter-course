c657fe7a1b022e7ed5b5050d17124e7efb73da30
John Coppola <jcoppola@insiten.com>
2021-06-13 16:45:00 -0400

    clears boilerplate content and sets up entry point and AppBar

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+    );
+  }
+}
