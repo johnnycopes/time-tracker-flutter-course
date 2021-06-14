d97ce7041b675c40425f20a24f98c74e1f24bdae
John Coppola <jcoppola@insiten.com>
2021-06-14 08:52:19 -0400

    sets up dummy boxes to represent sign in buttons layout

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -8,6 +8,39 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
+      body: _buildContent(),
     );
   }
+}
+
+Widget _buildContent() {
+  return Padding(
+    padding: EdgeInsets.all(16.0),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          color: Colors.orange,
+          child: SizedBox(
+            height: 100.0,
+          ),
+        ),
+        SizedBox(height: 8.0),
+        Container(
+          color: Colors.red,
+          child: SizedBox(
+            height: 100.0,
+          ),
+        ),
+        SizedBox(height: 8.0),
+        Container(
+          color: Colors.purple,
+          child: SizedBox(
+            height: 100.0,
+          ),
+        ),
+      ],
+    ),
+  );
 }
