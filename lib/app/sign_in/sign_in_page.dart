c35a462c68204c38e5908b49b97ed330436aad58
John Coppola <jcoppola@insiten.com>
2021-06-20 10:39:24 -0400

    installs Firebase and Firebase auth; gets anonymous sign in function working

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
 
 class SignInPage extends StatelessWidget {
+  Future<void> _signInAnonymously() async {
+    try {
+      final userCredentials = await FirebaseAuth.instance.signInAnonymously();
+      print("user creds: ${userCredentials.user?.uid}");
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,61 +24,61 @@ class SignInPage extends StatelessWidget {
       backgroundColor: Colors.grey.shade200,
     );
   }
-}
 
-Widget _buildContent() {
-  return Padding(
-    padding: EdgeInsets.all(16.0),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Text(
-          'Sign In',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.w600,
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
           ),
-        ),
-        SizedBox(height: 48.0),
-        SizedBox(height: 8.0),
-        SocialSignInButton(
-          text: 'Sign in with Google',
-          assetName: 'images/google-logo.png',
-          textColor: Colors.black87,
-          color: Colors.white,
-          onPressed: () => print("google!"),
-        ),
-        SizedBox(height: 8.0),
-        SocialSignInButton(
-          text: 'Sign in with Facebook',
-          assetName: 'images/facebook-logo.png',
-          textColor: Colors.white,
-          color: Color(0xFF334D92),
-          onPressed: () => print("facebook!"),
-        ),
-        SizedBox(height: 8.0),
-        SignInButton(
-          text: 'Sign in with email',
-          textColor: Colors.white,
-          color: Colors.teal,
-          onPressed: () => print("email!"),
-        ),
-        SizedBox(height: 8.0),
-        Text(
-          'or',
-          style: TextStyle(fontSize: 14.0, color: Colors.black87),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 8.0),
-        SignInButton(
-          text: 'Go anonymous',
-          textColor: Colors.black,
-          color: Colors.lime.shade300,
-          onPressed: () => print("anonymous!"),
-        ),
-      ],
-    ),
-  );
+          SizedBox(height: 48.0),
+          SizedBox(height: 8.0),
+          SocialSignInButton(
+            text: 'Sign in with Google',
+            assetName: 'images/google-logo.png',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () => print("google!"),
+          ),
+          SizedBox(height: 8.0),
+          SocialSignInButton(
+            text: 'Sign in with Facebook',
+            assetName: 'images/facebook-logo.png',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () => print("facebook!"),
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal,
+            onPressed: () => print("email!"),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime.shade300,
+            onPressed: _signInAnonymously,
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app/sign_in/sign_in_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool _initialized = false;
+  bool _error = false;
+
+  Future<void> initializeFlutterFire() async {
+    try {
+      final myApp = await Firebase.initializeApp();
+      print(myApp);
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_error) {
+      // return SomethingWentWrong();
+      print("error");
+    }
+
+    if (!_initialized) {
+      // return Loading();
+      print("loading");
+    }
+
     return MaterialApp(
       title: 'Time Tracker',
       theme: ThemeData(
